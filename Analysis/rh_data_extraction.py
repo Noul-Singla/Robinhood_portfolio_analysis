01b56bdee698488f4cba5312055ba61e8b8f705c
Noul Singla <singla.n@husky.neu.edu>
2020-11-21 22:58:00 -0500

    Added some code for visualization and some functions

--- Analysis/rh_data_extraction.py
+++ Analysis/rh_data_extraction.py
@@ -1,43 +1,68 @@
+from Analysis.yahoo_finance import get_yahoo_tickr_data
 from robin_stocks import authentication, account
 import pandas as pd
 import plotly.express as px
+
 """
 Our purpose is to take that data and build a way to visualize and analyse it in more user friendly and detailed manner
 """
-authentication.login()
-portfolio = account.build_holdings(with_dividends=False)
-for item in portfolio:
-    portfolio[item]['tickr'] = item
+def get_RHportfolio_data():
+    portfolio = account.build_holdings(with_dividends=False)
+    for item in portfolio:
+        portfolio[item]['tickr'] = item
 
-portfolio_clean = list()
-for item in portfolio:
-    portfolio_clean.append(portfolio[item])
+    portfolio_clean = list()
+    for item in portfolio:
+        portfolio_clean.append(portfolio[item])
 
-portfolio_clean[1]
-portfolio_df = pd.DataFrame(portfolio_clean)
+    portfolio_clean[1]
+    portfolio_df = pd.DataFrame(portfolio_clean)
 
-portfolio_df['quantity'] = pd.to_numeric(portfolio_df.quantity)
-portfolio_df['average_buy_price'] = pd.to_numeric(portfolio_df.average_buy_price)
-portfolio_df['total_buy_cost'] = round(portfolio_df['quantity']*portfolio_df['average_buy_price'],2)
-total_investment = sum(portfolio_df['total_buy_cost'])
-portfolio_df['percent_share'] = round((portfolio_df['total_buy_cost']/total_investment)*100, 2)
+    portfolio_df['quantity'] = pd.to_numeric(portfolio_df.quantity)
+    portfolio_df['average_buy_price'] = pd.to_numeric(portfolio_df.average_buy_price)
+    portfolio_df['total_buy_cost'] = round(portfolio_df['quantity']*portfolio_df['average_buy_price'],2)
+    total_investment = sum(portfolio_df['total_buy_cost'])
+    portfolio_df['percent_share'] = round((portfolio_df['total_buy_cost']/total_investment)*100, 2)
 
-fig = px.pie(portfolio_df, values='percent_share', names='tickr', title='Investment Stocks Distribution',
-             hover_name ='tickr',
-             hover_data=["name", "total_buy_cost"],hole=.4,
-             labels={'total_buy_cost': 'Cost ', 'name': 'Name ', 'percent_share' : 'Share (%) ', 'tickr': 'Tickr '})
-fig.update_traces(textposition='inside', textinfo='label')
-fig.show()
+    return portfolio_df
 
-
-
-fig = px.bar(portfolio_df, y='percent_share', x='tickr', title='Investment Stocks Distribution',
-             hover_name ='tickr',
-             hover_data=["name", "total_buy_cost"],
-             labels={'total_buy_cost': 'Cost ', 'name': 'Name', 'percent_share' : 'Share (%)', 'tickr': 'Tickr'})
-fig.show()
+def display_portfolio_pie(portfolio_df):
+    fig = px.pie(portfolio_df, values='percent_share', names='tickr', title='Investment Stocks Distribution',
+                 hover_name ='tickr',
+                 hover_data=["name", "total_buy_cost"],hole=.4,
+                 labels={'total_buy_cost': 'Cost ', 'name': 'Name ', 'percent_share' : 'Share (%) ', 'tickr': 'Tickr '})
+    fig.update_traces(textposition='inside', textinfo='label')
+    fig.show()
 
 
-portfolio_df.loc[1]
+def display_portfolio_bar(portfolio_df):
+    fig = px.bar(portfolio_df, y='percent_share', x='tickr', title='Investment Stocks Distribution',
+                 hover_name ='tickr',
+                 hover_data=["name", "total_buy_cost"],
+                 labels={'total_buy_cost': 'Cost ', 'name': 'Name', 'percent_share' : 'Share (%)', 'tickr': 'Tickr'})
+    fig.update_layout({'plot_bgcolor': 'rgba(0, 0, 0, 0)','paper_bgcolor': 'rgba(0, 0, 0, 0)',})
+    fig.show()
 
 
+def display_tickr_line_chart(input_df):
+    y_axis = "close"
+    fig = px.line(input_df, x="timestamp", y=y_axis, title='Data for stocks', color ='tickr')
+    fig.update_layout({'plot_bgcolor': 'rgba(0, 0, 0, 0)','paper_bgcolor': 'rgba(0, 0, 0, 0)',})
+    fig.show()
+
+
+def get_tickr_data(tickr_list, interval = '2m', range = '1d'):
+    yahoo_data = pd.DataFrame()
+    for tickr in tickr_list:
+        yahoo_data = yahoo_data.append(get_yahoo_tickr_data(tickr, interval, range))
+    return yahoo_data
+
+#def if __name__ == '__main__':
+def main():
+    authentication.login()
+    portfolio_df = get_RHportfolio_data()
+    display_portfolio_pie(portfolio_df)
+    display_portfolio_bar(portfolio_df)
+    tickr_list = ['NIO','XPEV']
+    yahoo_data = get_tickr_data(tickr_list, range = '10d')
+    display_tickr_line_chart(yahoo_data)
--- Analysis/yahoo_finance.py
+++ Analysis/yahoo_finance.py
@@ -3,29 +3,35 @@ import json
 import pandas as pd
 from datetime import datetime
 
-tickr = 'AAPL'
-interval ='2m'
-range = '1d'
-yahoo_url = 'https://query1.finance.yahoo.com/v8/finance/chart/{}' \
-            '?region=US&lang=en-US&includePrePost=false&interval={}&range={}' \
-            '&corsDomain=finance.yahoo.com&.tsrc=finance'.format(tickr, interval, range)
 
-yahoo_response = requests.get(yahoo_url)
-yahoo_response_string = yahoo_response.text
-yahoo_response_json = json.loads(yahoo_response_string)
+def get_rounded_list(input_list,decimal):
+    return_list = [round(num, 3) for num in input_list]
+    return return_list
 
-yahoo_tickr_data = dict()
-yahoo_tickr__meta = yahoo_response_json['chart']['result'][0]['meta']
-yahoo_tickr_data['timestamp_utc'] = yahoo_response_json['chart']['result'][0]['timestamp']
-yahoo_response_indicators = yahoo_response_json['chart']['result'][0]['indicators']
-yahoo_tickr_data['close'] = yahoo_response_indicators['quote'][0]['close']
-yahoo_tickr_data['open'] = yahoo_response_indicators['quote'][0]['open']
-yahoo_tickr_data['high'] = yahoo_response_indicators['quote'][0]['high']
-yahoo_tickr_data['volume'] = yahoo_response_indicators['quote'][0]['volume']
-yahoo_tickr_data['low'] = yahoo_response_indicators['quote'][0]['low']
 
-yahoo_tickr_df = pd.DataFrame(yahoo_tickr_data)
+def get_yahoo_tickr_data(tickr,interval = '2m', range ='1d'):
 
-yahoo_tickr_df['timestamp'] = yahoo_tickr_df['timestamp_utc'].map(lambda x: datetime.fromtimestamp(x).strftime('%Y-%m-%d %H:%M:%S'))
+    yahoo_url = 'https://query1.finance.yahoo.com/v8/finance/chart/{}' \
+                '?region=US&lang=en-US&includePrePost=false&interval={}&range={}' \
+                '&corsDomain=finance.yahoo.com&.tsrc=finance'.format(tickr, interval, range)
 
-yahoo_tickr_df
+    yahoo_response = requests.get(yahoo_url)
+    yahoo_response_string = yahoo_response.text
+    yahoo_response_json = json.loads(yahoo_response_string)
+
+    yahoo_tickr_data = dict()
+    yahoo_tickr__meta = yahoo_response_json['chart']['result'][0]['meta']
+    yahoo_tickr_data['tickr'] = tickr
+    yahoo_tickr_data['timestamp_utc'] = yahoo_response_json['chart']['result'][0]['timestamp']
+    yahoo_response_indicators = yahoo_response_json['chart']['result'][0]['indicators']
+    yahoo_tickr_data['close'] = get_rounded_list(yahoo_response_indicators['quote'][0]['close'], 3)
+    yahoo_tickr_data['open'] = get_rounded_list(yahoo_response_indicators['quote'][0]['open'], 3)
+    yahoo_tickr_data['high'] = get_rounded_list(yahoo_response_indicators['quote'][0]['high'], 3)
+    yahoo_tickr_data['volume'] = get_rounded_list(yahoo_response_indicators['quote'][0]['volume'], 3)
+    yahoo_tickr_data['low'] = get_rounded_list(yahoo_response_indicators['quote'][0]['low'], 3)
+
+    yahoo_tickr_df = pd.DataFrame(yahoo_tickr_data)
+
+    yahoo_tickr_df['timestamp'] = yahoo_tickr_df['timestamp_utc'].map(lambda x: datetime.fromtimestamp(x).strftime('%Y-%m-%d %H:%M:%S'))
+
+    return yahoo_tickr_df
