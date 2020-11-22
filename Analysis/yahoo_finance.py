d117e6d7201c91a3779f86e90c3d1c4da766f759
Noul Singla <singla.n@husky.neu.edu>
2020-11-21 21:12:54 -0500

    adding requirements file

--- Analysis/yahoo_finance.py
+++ Analysis/yahoo_finance.py
@@ -1,26 +1,31 @@
 import requests
 import json
+import pandas as pd
+from datetime import datetime
 
 tickr = 'AAPL'
 interval ='2m'
 range = '1d'
-yahoo_url = 'https://query1.finance.yahoo.com/v8/finance/chart/AAPL?region=US&lang=en-US&includePrePost=false&interval=2m&range=1d&corsDomain=finance.yahoo.com&.tsrc=finance'
+yahoo_url = 'https://query1.finance.yahoo.com/v8/finance/chart/{}' \
+            '?region=US&lang=en-US&includePrePost=false&interval={}&range={}' \
+            '&corsDomain=finance.yahoo.com&.tsrc=finance'.format(tickr, interval, range)
+
 yahoo_response = requests.get(yahoo_url)
 yahoo_response_string = yahoo_response.text
 yahoo_response_json = json.loads(yahoo_response_string)
-yahoo_response_json['chart']['result'][0].keys()
-yahoo_response_meta = yahoo_response_json['chart']['result'][0]['meta']
-yahoo_response_timestamp = yahoo_response_json['chart']['result'][0]['timestamp']
+
+yahoo_tickr_data = dict()
+yahoo_tickr__meta = yahoo_response_json['chart']['result'][0]['meta']
+yahoo_tickr_data['timestamp_utc'] = yahoo_response_json['chart']['result'][0]['timestamp']
 yahoo_response_indicators = yahoo_response_json['chart']['result'][0]['indicators']
-yahoo_response_indicators['quote'][0].keys()
-yahoo_response_close = yahoo_response_indicators['quote'][0]['close']
-yahoo_response_open = yahoo_response_indicators['quote'][0]['open']
-yahoo_response_high = yahoo_response_indicators['quote'][0]['high']
-yahoo_response_volume = yahoo_response_indicators['quote'][0]['volume']
-yahoo_response_low = yahoo_response_indicators['quote'][0]['low']
+yahoo_tickr_data['close'] = yahoo_response_indicators['quote'][0]['close']
+yahoo_tickr_data['open'] = yahoo_response_indicators['quote'][0]['open']
+yahoo_tickr_data['high'] = yahoo_response_indicators['quote'][0]['high']
+yahoo_tickr_data['volume'] = yahoo_response_indicators['quote'][0]['volume']
+yahoo_tickr_data['low'] = yahoo_response_indicators['quote'][0]['low']
 
-yahoo_response_close[1]
-yahoo_response_open[1]
-yahoo_response_high[1]
-yahoo_response_low[2]
+yahoo_tickr_df = pd.DataFrame(yahoo_tickr_data)
 
+yahoo_tickr_df['timestamp'] = yahoo_tickr_df['timestamp_utc'].map(lambda x: datetime.fromtimestamp(x).strftime('%Y-%m-%d %H:%M:%S'))
+
+yahoo_tickr_df
